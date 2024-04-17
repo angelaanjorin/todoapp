8f7ce5b99eca053f5272d8112c5c4b375c168bf0
angelaanjorin <163406589+angelaanjorin@users.noreply.github.com>
2024-04-17 12:46:49 +0200

    Define elements in script.js, add an eventlistener to the todoButton and add a function to create the elements of the todolist

---
+++ script.js
@@ -0,0 +1,31 @@
+//Selectors
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const todoButton = document.getElementById("todo-button");
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+//Functions
+
+function addTodo(event){
+    //Todo DIv
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //CHECK TRASH BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APEND TO LIST
+    listContainer.appendChild(todoDiv);
+}
