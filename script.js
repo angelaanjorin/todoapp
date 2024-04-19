e483fee14c2eaa25e9511ae167c58b858350bde1
angelaanjorin <163406589+angelaanjorin@users.noreply.github.com>
2024-04-19 20:02:40 +0200

    Add code for render todos in script.js and update index.html

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Selectors
 const form = document.getElementById("todoform");
 const todoInput = document.getElementById("newtodo");
+const todosList = document.getElementById("todos-list");
 
 //VARS
 let todos = [];
@@ -9,6 +10,7 @@ form.addEventListener('submit', function (event) {
     event.preventDefault();
     
     saveTodo();
+    renderTodos();
 })
 
 //SAVE TODO
@@ -19,22 +21,40 @@ form.addEventListener('submit', function (event) {
         const isEmpty = todoValue === '';
         
         //check for duplicate todos
-        const isDuplicate = todos.some((todo) => todo.value === todoValue);
+        const isDuplicate = todos.some((todo) => todo.value.toUpperCase() === todoValue.toUpperCase());
 
 
         if (isEmpty) {
             alert("Todo`s input is empty");
         } else if (isDuplicate) {
-            alert("Todo already exists!")
+            alert("Todo already exists!");
         }
-        else {const todo = {
-            value : todoValue,
-            checked : false,
-        };
-       todos.push (todo);
-        console.log (todos);
+        else {
+       todos.push ({
+        value : todoValue,
+        checked : false,
+    });
+       todoInput.value = "";
     };
 }
+
+//  RENDER TODOS
+    function renderTodos(){
+        //CLEAR ELEMENT BEFORE A RE-RENDER
+            todosList.innerHTML = "";
+
+            //RENDER TODOS
+        todos.forEach((todo, index) => {
+            todosList.innerHTML += `
+            <div class="todo" id=${index}>
+            <i class= "fa ${todo.checked ? 'fa-circle-check' : 'regular fa-circle'}" ></i>
+            <p class="">${todo.value}</p>
+            <i class="fa-solid fa-pen-to-square"></i>
+            <i class="fas fa-trash"></i>
+          </div>
+          `;
+        });
+    }
 //Event Listeners
 
 //Functions
