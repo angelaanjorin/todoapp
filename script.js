43fe5f60446b2255ab7a4d9c6bbdc6344a70b21e
angelaanjorin <163406589+angelaanjorin@users.noreply.github.com>
2024-04-19 19:11:02 +0200

    updated index.html, style.css and script.js

--- script.js
+++ script.js
@@ -1,49 +1,40 @@
 //Selectors
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-const todoButton = document.getElementById("todo-button");
-const todoList = document.getElementById("todo-list");
+const form = document.getElementById("todoform");
+const todoInput = document.getElementById("newtodo");
 
-//Event Listeners
-todoButton.addEventListener('click', addTodo);
-listContainer.addEventListener('click', deleteCheck);
-//Functions
+//VARS
+let todos = [];
+//FORM SUBMIT
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    saveTodo();
+})
 
-function addTodo(event){
-    //Todo DIv
-    const todoDiv = document.createElement("div");
-    todoDiv.classList.add("todo");
-    //Create LI
-    const newTodo = document.createElement('li');
-    newTodo.innerText = inputBox.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-    //CHECK MARK BUTTON
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    //CHECK TRASH BUTTON
-    const trashButton = document.createElement('button');
-    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-    trashButton.classList.add("trash-btn");
-    todoDiv.appendChild(trashButton);
-    //APEND TO LIST
-    listContainer.appendChild(todoDiv);
-    //CLEAR INPUTBOX VALUE
-    inputBox.value = "";
+//SAVE TODO
+    function saveTodo() {
+        const todoValue = todoInput.value
+        
+        //check if the todo is empty
+        const isEmpty = todoValue === '';
+        
+        //check for duplicate todos
+        const isDuplicate = todos.some((todo) => todo.value === todoValue);
+
+
+        if (isEmpty) {
+            alert("Todo`s input is empty");
+        } else if (isDuplicate) {
+            alert("Todo already exists!")
+        }
+        else {const todo = {
+            value : todoValue,
+            checked : false,
+        };
+       todos.push (todo);
+        console.log (todos);
+    };
 }
+//Event Listeners
 
-function deleteCheck(e){
-    const item = e.target;
-    //DELETE TODO
-    if (item.classList[0] === "trash-btn") {
-        const todo = item.parentElement;
-        todo.remove();
-    }
-
-    //CHECK MARK    
-    if (item.classList[0] === "complete-btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completed");}
-    }
+//Functions
